fbc82673b320cd44c475f2cecafdf306807dccc0
Kierra M <k.merriman32@hotmail.com>
2019-08-24 02:40:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
     elsif array.length == 3 
       return  "#{array[0]}, #{array[1]}, and #{array[2]}"
     elsif array.length > 3 
-      array[-1].insert("and ")
+      array[-1].insert(0, "and ")
     end
 end 
