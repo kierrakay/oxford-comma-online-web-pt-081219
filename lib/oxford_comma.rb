140f70a5c2107c52c8319bebf03e8675f15e644e
Kierra M <k.merriman32@hotmail.com>
2019-08-24 02:37:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
       return array[0]
     elsif array.length == 2
       return "#{array[0]} and #{array[1]}"
-    elsif array.length > 3 
-      return  "#{array[0]}, #{array[1]}, ..., and #{array}"
+    elsif array.length == 3 
+      return  "#{array[0]}, #{array[1]}, and #{array[2]}"
+    
     end
 end 
