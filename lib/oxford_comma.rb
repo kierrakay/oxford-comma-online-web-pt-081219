bce8a19faed982c15dfcee522b2dd4bb20590447
Kierra M <k.merriman32@hotmail.com>
2019-08-24 02:18:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-return array.first
+if array <= 
+  array
 end
+
+
+//* ef oxford_comma(array)
+	if ARRAY HAS ELEMENT
+      return THE ELEMENT
+    elsif ARRAY HAS TWO ELEMENTS
+      return THE TWO ELEMENTS FORMATTED "Name1 and Name2"
+    elsif ARRAY HAS > 2 ELEMENTS
+      return THE ELEMENTS FORMATTED LIKE "Name1, name2, ..., and . Namen"
+    end
+end *//
